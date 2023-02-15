b0e5136f99f2ad54aa43e93178e55362facd9f05
ishika <ishikaishu2000@gmail.com>
2023-02-15 15:07:13 -0800

    updated settings.py

--- csc648_team1/settings.py
+++ csc648_team1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-h
+
 ALLOWED_HOSTS = ['django', '127.0.0.1']
 
 
