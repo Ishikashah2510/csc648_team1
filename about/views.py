7b72404f832edce484831f5b74cfe7436511d0e4
ishika <ishikaishu2000@gmail.com>
2023-02-15 15:40:03 -0800

    updated view

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 import os
+import logging
 
 # Create your views here.
 
@@ -8,4 +9,5 @@ import os
 def render_about(request):
     details = get_about_details()
     print("os.getcwd() idhar milega: ", os.getcwd())
+    logging.info("os.getcwd() idhar milega: ", os.getcwd())
     return render(request, 'index.html')
