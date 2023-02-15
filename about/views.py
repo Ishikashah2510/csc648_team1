a22e442009486eb1f10760b8435631c641963b59
ishika <ishikaishu2000@gmail.com>
2023-02-15 15:21:40 -0800

    updated Dockerfile

--- about/views.py
+++ about/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import *
+import os
 
 # Create your views here.
 
 
 def render_about(request):
     details = get_about_details()
+    print("os.getcwd() idhar milega: ", os.getcwd())
     return render(request, 'index.html')
--- csc648_team1/settings.py
+++ csc648_team1/settings.py
@@ -16,6 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print("base directory: ", BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
