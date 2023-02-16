afe6fdb7c3fe954924fd17708fea52e74421659f
ishika <ishikaishu2000@gmail.com>
2023-02-15 19:44:30 -0800

    commit

--- csc648_team1/settings.py
+++ csc648_team1/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'csc648_team1/my-app/build/static')
 ]
 
-STATIC_ROOT = "static/"
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
