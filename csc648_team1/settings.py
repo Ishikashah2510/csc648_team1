ea73126bcd92c17a8ea857a91d609291642b82ac
ishika <ishikaishu2000@gmail.com>
2023-02-15 18:48:25 -0800

    commit

--- csc648_team1/settings.py
+++ csc648_team1/settings.py
@@ -57,8 +57,7 @@ ROOT_URLCONF = 'csc648_team1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'csc648_team1/my-app/build')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'csc648_team1/my-app/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
