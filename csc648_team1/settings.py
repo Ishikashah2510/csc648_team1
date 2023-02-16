3f99b38946b918e3ca10751f42fee9c170a8b702
ishika <ishikaishu2000@gmail.com>
2023-02-15 19:31:59 -0800

    commit

--- csc648_team1/settings.py
+++ csc648_team1/settings.py
@@ -125,12 +125,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'csc648_team1/my-app/build/static/css/'),
-    os.path.join(BASE_DIR, 'csc648_team1/my-app/build/static/js/'),
+    os.path.join(BASE_DIR, 'csc648_team1/my-app/build/static')
 ]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'csc648_team1/my-app/build/static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
